b8609c829026764fb139b0b49b81181183015c48
Kartik <9415kks@gmail.com>
2022-01-13 23:31:12 +0530

    Added some changes in the source file

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./app.css";
+
+function App() {
+  return (
+    <div className="App">
+      <img
+        src="https://images.pexels.com/photos/9371782/pexels-photo-9371782.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        alt=""
+        width="400"
+        height="500"
+      />
+      <canvas width="400" height="500" />
+    </div>
+  );
+}
+
+export default App;
