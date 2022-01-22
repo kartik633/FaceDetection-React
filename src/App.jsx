6b26fbd15746b15deb98d0c2d7bfe3f4b4de6d99
Kartik <9415kks@gmail.com>
2022-01-22 19:44:35 +0530

    Face Detection

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,28 @@ function App() {
   const imgRef = useRef();
   const canvasRef = useRef();
 
+  const handleImage = async () => {
+    const detections = await faceapi
+      .detectAllFaces(imgRef.current, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions();
+
+    // console.log(detections);
+
+    canvasRef.current.innerHtml = faceapi.createCanvasFromMedia(imgRef.current);
+    faceapi.matchDimensions(canvasRef.current, {
+      width: 400,
+      height: 500,
+    });
+
+    const resized = faceapi.resizeResults(detections, {
+      width: 400,
+      height: 500,
+    });
+    faceapi.draw.drawDetections(canvasRef.current, resized);
+    faceapi.draw.drawFaceExpressions(canvasRef.current, resized);
+  };
+
   useEffect(() => {
     const loadModels = () => {
       Promise.all([
@@ -14,16 +36,16 @@ function App() {
         faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
         faceapi.nets.faceExpressionNet.loadFromUri("/models"),
       ])
-        .then(console.log("its done"))
+        .then(handleImage)
         .catch((e) => console.log(e));
     };
 
     imgRef.current && loadModels();
-
   }, []);
   return (
     <div className="App">
       <img
+        crossOrigin="anonymous"
         ref={imgRef}
         src="https://images.pexels.com/photos/9371782/pexels-photo-9371782.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
         alt=""
