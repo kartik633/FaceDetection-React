230acfa7ca303e3d510175fea16826f7959bebe6
Kartik <9415kks@gmail.com>
2022-01-28 11:22:40 +0530

    Passing the height and width from app.jsx to newpost.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./app.css";
 import Navbar from "./components/Navbar";
+import NewPost from "./components/NewPost";
 
 function App() {
   const [file, setFile] = useState();
@@ -27,46 +28,50 @@ function App() {
   return (
     <div>
       <Navbar />
-      <div className="newPostCard">
-        <div className="addPost">
-          <img
-            src="https://images.pexels.com/photos/9371782/pexels-photo-9371782.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            alt=""
-            className="avatar"
-          />
-          <div className="postForm">
-            <input
-              type="text"
-              placeholder="What's on your mind?"
-              className="postInput"
+      {image ? (
+        <NewPost image={image} />
+      ) : (
+        <div className="newPostCard">
+          <div className="addPost">
+            <img
+              src="https://images.pexels.com/photos/9371782/pexels-photo-9371782.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
+              className="avatar"
             />
-            <label htmlFor="file">
-              <img
-                className="addImg"
-                src="https://cdn.icon-icons.com/icons2/564/PNG/512/Add_Image_icon-icons.com_54218.png"
-                alt=""
+            <div className="postForm">
+              <input
+                type="text"
+                placeholder="What's on your mind?"
+                className="postInput"
               />
-              <img
-                className="addImg"
-                src="https://icon-library.com/images/maps-icon-png/maps-icon-png-5.jpg"
-                alt=""
+              <label htmlFor="file">
+                <img
+                  className="addImg"
+                  src="https://cdn.icon-icons.com/icons2/564/PNG/512/Add_Image_icon-icons.com_54218.png"
+                  alt=""
+                />
+                <img
+                  className="addImg"
+                  src="https://icon-library.com/images/maps-icon-png/maps-icon-png-5.jpg"
+                  alt=""
+                />
+                <img
+                  className="addImg"
+                  src="https://d29fhpw069ctt2.cloudfront.net/icon/image/84451/preview.svg"
+                  alt=""
+                />
+                <button>Send</button>
+              </label>
+              <input
+                onChange={(e) => setFile(e.target.files[0])}
+                id="file"
+                style={{ display: "none" }}
+                type="file"
               />
-              <img
-                className="addImg"
-                src="https://d29fhpw069ctt2.cloudfront.net/icon/image/84451/preview.svg"
-                alt=""
-              />
-              <button>Send</button>
-            </label>
-            <input
-              onChange={(e) => setFile(e.target.files[0])}
-              id="file"
-              style={{ display: "none" }}
-              type="file"
-            />
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
